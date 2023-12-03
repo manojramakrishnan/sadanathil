dcf9731b5e4965f1eea766f9cd039fc31fc55db8
Manoj Kumar <manojkumar@CPM-MANOJ.local>
2023-12-03 10:57:41 +0530

    deleted all the files

--- src/main/java/com/sadanathil/services/ITeacherService.java
+++
@@ -1,5 +0,0 @@
-package com.sadanathil.services;
-
-public interface ITeacherService {
-
-}
--- src/main/java/com/sadanathil/services/TeacherServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.sadanathil.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class TeacherServiceImpl implements ITeacherService{
-}
