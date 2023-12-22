bb850b5a2fbf5bd21f50f681888529be29d0e687
akhil murali <akhilmurali662@gmail.com>
2023-12-22 09:43:07 +0530

    added dto

--- src/main/java/com/sadanathil/dto/LoginDto.java
+++ src/main/java/com/sadanathil/dto/LoginDto.java
@@ -1,8 +1,11 @@
 package com.sadanathil.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class LoginDto {
 
 	private Long tcNo;
